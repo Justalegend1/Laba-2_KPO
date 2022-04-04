4cc96b0d41163b673890bceeec091acf4e7b9359
Никита Копылов <nekopylov@edu.hse.ru>
2022-04-04 13:40:13 +0500

    Добавьте файлы проекта.

---
+++ TreeWithSQL/UpdateCourse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TreeWithSQL
+{
+    public partial class UpdateCourse : Form
+    {
+        string id;
+        string facultyId;
+
+        public UpdateCourse(string id, string facultyId)
+        {
+            InitializeComponent();
+            this.id = id;
+            this.facultyId = facultyId;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TreeWithSQLLabDB;Integrated Security=True;");
+            conn.Open();
+            var sqlq = $"UPDATE [dbo].[Course] SET [CourseNumber] = {textBox1.Text} WHERE [Id] = {id} AND [FacultyId] = {facultyId}";
+            var cmd = new SqlCommand(sqlq, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int w) || textBox1.Text == "")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели символ, вводите цифры!");
+                textBox1.Clear();
+            }
+        }
+    }
+}
